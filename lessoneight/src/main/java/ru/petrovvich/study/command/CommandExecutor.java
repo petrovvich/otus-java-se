802795c1665d93530bc16cd434001b31ac12af17
Aleksandr Romaniuk <407@smsfinance.ru>
2019-02-18 20:13:18 +0700

    HW8 fixes after cr

--- lessoneight/src/main/java/ru/petrovvich/study/command/CommandExecutor.java
+++ lessoneight/src/main/java/ru/petrovvich/study/command/CommandExecutor.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommandExecutor {
-    private List<Command> listCommands = new ArrayList();
+    private List<Command> listCommands = new ArrayList<>();
 
     public void addCommand(Command command) {
         listCommands.add(command);
     }
 
-    public void execute(Command command) {
-        listCommands.add(command);
-        command.execute();
+    public void execute() {
+        listCommands.forEach(Command::execute);
     }
 }
--- lessoneight/src/main/java/ru/petrovvich/study/model/ATMDepartment.java
+++ lessoneight/src/main/java/ru/petrovvich/study/model/ATMDepartment.java
@@ -1,5 +1,9 @@
 package ru.petrovvich.study.model;
 
+import ru.petrovvich.study.command.CommandExecutor;
+import ru.petrovvich.study.command.ReloadCommand;
+import ru.petrovvich.study.command.RestoreCommand;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +15,7 @@ import java.util.Map;
 public class ATMDepartment {
 
     private List<ATM> atms;
+    private CommandExecutor restoreCommandExecutor = new CommandExecutor();
 
     public ATMDepartment() {
         this.atms = new ArrayList<>();
@@ -18,13 +23,14 @@ public class ATMDepartment {
 
     public ATMDepartment(List<ATM> atms) {
         this.atms = atms;
+        atms.forEach(atm -> restoreCommandExecutor.addCommand(new RestoreCommand(atm)));
     }
 
     /**
      * Метод возвращает к исходному состоянию все банкоматы
      */
     public void resetAll() {
-        atms.forEach(ATM::onReload);
+        restoreCommandExecutor.execute();
     }
 
     /**
